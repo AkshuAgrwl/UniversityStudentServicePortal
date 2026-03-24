535ebb5e14aa2c392c9086a392a45f91ecbcd89b
Akshit Agarwal <139654279+AkshuAgrwl@users.noreply.github.com>
2026-03-25 03:41:08 +0530

    Migrate to mysql

--- src/lib/prismarc.ts
+++ src/lib/prismarc.ts
@@ -1,10 +1,12 @@
 import "dotenv/config";
-import { PrismaPg } from "@prisma/adapter-pg";
+// import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaMariaDb } from "@prisma/adapter-mariadb";
 import { PrismaClient } from "@/lib/prisma/client";
 
 const connectionString = `${process.env.DATABASE_URL}`;
 
-const adapter = new PrismaPg({ connectionString });
+// const adapter = new PrismaPg({ connectionString });
+const adapter = new PrismaMariaDb(connectionString);
 const prisma = new PrismaClient({ adapter });
 
 export { prisma };
