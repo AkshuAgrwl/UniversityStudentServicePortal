4922bb9699d42aea8157b284224ec9873d3f6a93
Akshit Agarwal <139654279+AkshuAgrwl@users.noreply.github.com>
2026-03-29 03:34:28 +0530

    Implement prisma singleton instance

--- src/lib/prismarc.ts
+++ src/lib/prismarc.ts
@@ -1,12 +1,21 @@
 import "dotenv/config";
-// import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaMariaDb } from "@prisma/adapter-mariadb";
 import { Prisma, PrismaClient } from "@/lib/prisma/client";
 
 const connectionString = `${process.env.DATABASE_URL}`;
 
-// const adapter = new PrismaPg({ connectionString });
-const adapter = new PrismaMariaDb(connectionString);
-const prisma = new PrismaClient({ adapter });
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+function createPrismaClient() {
+  console.log("Creating new Prisma Client instance...");
+
+  const adapter = new PrismaMariaDb(connectionString);
+  return new PrismaClient({ adapter });
+}
+
+const prisma = globalForPrisma.prisma || createPrismaClient();
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
 export { prisma, Prisma as TPrisma };
